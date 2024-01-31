966ec2239228a4bb2d6b95ae4ee290287b492640
Howard <howard_bb@hotmail.com>
2024-01-31 15:44:30 +0800

    feat: utils to turn string into kebab case

--- lib/utils/general.ts
+++ lib/utils/general.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toKebabCase(str: string) {
+  return str
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2") // insert hyphens between camelCase
+    .replace(/\s+/g, "-") // replace whitespace with hyphens
+    .replace(/-+/g, "-") // replace multiple hyphens with a single hyphen
+    .toLowerCase(); // convert to lowercase
+}
