11f7673e5759da378a7b008b42e6540e1b006904
Howard <howard_bb@hotmail.com>
2024-01-31 15:41:12 +0800

    feat: add default font face constants

---
+++ lib/constant/font-face.ts
@@ -0,0 +1,750 @@
+export const fontFaces = {
+  Arial: {
+    value: "Arial",
+    label: "Arial",
+  },
+  ArialItalic: {
+    value: "ArialItalic",
+    label: "Arial Italic",
+  },
+  ArialBold: {
+    value: "ArialBold",
+    label: "Arial Bold",
+  },
+  ArialBoldItalic: {
+    value: "ArialBoldItalic",
+    label: "Arial Bold Italic",
+  },
+  ArialBlack: {
+    value: "ArialBlack",
+    label: "Arial Black",
+  },
+  Bahnschrift: {
+    value: "Bahnschrift",
+    label: "Bahnschrift",
+  },
+  CalibriLight: {
+    value: "CalibriLight",
+    label: "Calibri Light",
+  },
+  CalibriLightItalic: {
+    value: "CalibriLightItalic",
+    label: "Calibri Light Italic",
+  },
+  Calibri: {
+    value: "Calibri",
+    label: "Calibri",
+  },
+  CalibriItalic: {
+    value: "CalibriItalic",
+    label: "Calibri Italic",
+  },
+  CalibriBold: {
+    value: "CalibriBold",
+    label: "Calibri Bold",
+  },
+  CalibriBoldItalic: {
+    value: "CalibriBoldItalic",
+    label: "Calibri Bold Italic",
+  },
+  Cambria: {
+    value: "Cambria",
+    label: "Cambria",
+  },
+  CambriaItalic: {
+    value: "CambriaItalic",
+    label: "Cambria Italic",
+  },
+  CambriaBold: {
+    value: "CambriaBold",
+    label: "Cambria Bold",
+  },
+  CambriaBoldItalic: {
+    value: "CambriaBoldItalic",
+    label: "Cambria Bold Italic",
+  },
+  CambriaMath: {
+    value: "CambriaMath",
+    label: "Cambria Math",
+  },
+  CandaraLight: {
+    value: "CandaraLight",
+    label: "Candara Light",
+  },
+  CandaraLightItalic: {
+    value: "CandaraLightItalic",
+    label: "Candara Light Italic",
+  },
+  Candara: {
+    value: "Candara",
+    label: "Candara",
+  },
+  CandaraItalic: {
+    value: "CandaraItalic",
+    label: "Candara Italic",
+  },
+  CandaraBold: {
+    value: "CandaraBold",
+    label: "Candara Bold",
+  },
+  CandaraBoldItalic: {
+    value: "CandaraBoldItalic",
+    label: "Candara Bold Italic",
+  },
+  ComicSansMS: {
+    value: "ComicSansMS",
+    label: "Comic Sans MS",
+  },
+  ComicSansMSItalic: {
+    value: "ComicSansMSItalic",
+    label: "Comic Sans MS Italic",
+  },
+  ComicSansMSBold: {
+    value: "ComicSansMSBold",
+    label: "Comic Sans MS Bold",
+  },
+  ComicSansMSBoldItalic: {
+    value: "ComicSansMSBoldItalic",
+    label: "Comic Sans MS Bold Italic",
+  },
+  Consolas: {
+    value: "Consolas",
+    label: "Consolas",
+  },
+  ConsolasItalic: {
+    value: "ConsolasItalic",
+    label: "Consolas Italic",
+  },
+  ConsolasBold: {
+    value: "ConsolasBold",
+    label: "Consolas Bold",
+  },
+  ConsolasBoldItalic: {
+    value: "ConsolasBoldItalic",
+    label: "Consolas Bold Italic",
+  },
+  Constantia: {
+    value: "Constantia",
+    label: "Constantia",
+  },
+  ConstantiaItalic: {
+    value: "ConstantiaItalic",
+    label: "Constantia Italic",
+  },
+  ConstantiaBold: {
+    value: "ConstantiaBold",
+    label: "Constantia Bold",
+  },
+  ConstantiaBoldItalic: {
+    value: "ConstantiaBoldItalic",
+    label: "Constantia Bold Italic",
+  },
+  CorbelLight: {
+    value: "CorbelLight",
+    label: "Corbel Light",
+  },
+  CorbelLightItalic: {
+    value: "CorbelLightItalic",
+    label: "Corbel Light Italic",
+  },
+  Corbel: {
+    value: "Corbel",
+    label: "Corbel",
+  },
+  CorbelItalic: {
+    value: "CorbelItalic",
+    label: "Corbel Italic",
+  },
+  CorbelBold: {
+    value: "CorbelBold",
+    label: "Corbel Bold",
+  },
+  CorbelBoldItalic: {
+    value: "CorbelBoldItalic",
+    label: "Corbel Bold Italic",
+  },
+  CourierNew: {
+    value: "CourierNew",
+    label: "Courier New",
+  },
+  CourierNewItalic: {
+    value: "CourierNewItalic",
+    label: "Courier New Italic",
+  },
+  CourierNewBold: {
+    value: "CourierNewBold",
+    label: "Courier New Bold",
+  },
+  CourierNewBoldItalic: {
+    value: "CourierNewBoldItalic",
+    label: "Courier New Bold Italic",
+  },
+  Ebrima: {
+    value: "Ebrima",
+    label: "Ebrima",
+  },
+  EbrimaBold: {
+    value: "EbrimaBold",
+    label: "Ebrima Bold",
+  },
+  FranklinGothicMedium: {
+    value: "FranklinGothicMedium",
+    label: "Franklin Gothic Medium",
+  },
+  FranklinGothicMediumItalic: {
+    value: "FranklinGothicMediumItalic",
+    label: "Franklin Gothic Medium Italic",
+  },
+  Gabriola: {
+    value: "Gabriola",
+    label: "Gabriola",
+  },
+  Gadugi: {
+    value: "Gadugi",
+    label: "Gadugi",
+  },
+  GadugiBold: {
+    value: "GadugiBold",
+    label: "Gadugi Bold",
+  },
+  Georgia: {
+    value: "Georgia",
+    label: "Georgia",
+  },
+  GeorgiaItalic: {
+    value: "GeorgiaItalic",
+    label: "Georgia Italic",
+  },
+  GeorgiaBold: {
+    value: "GeorgiaBold",
+    label: "Georgia Bold",
+  },
+  GeorgiaBoldItalic: {
+    value: "GeorgiaBoldItalic",
+    label: "Georgia Bold Italic",
+  },
+  HoloLensMDL2Assets: {
+    value: "HoloLensMDL2Assets",
+    label: "HoloLens MDL2 Assets",
+  },
+  Impact: {
+    value: "Impact",
+    label: "Impact",
+  },
+  InkFree: {
+    value: "InkFree",
+    label: "Ink Free",
+  },
+  JavaneseText: {
+    value: "JavaneseText",
+    label: "Javanese Text",
+  },
+  LeelawadeeUI: {
+    value: "LeelawadeeUI",
+    label: "Leelawadee UI",
+  },
+  LeelawadeeUISemilight: {
+    value: "LeelawadeeUISemilight",
+    label: "Leelawadee UI Semilight",
+  },
+  LeelawadeeUIBold: {
+    value: "LeelawadeeUIBold",
+    label: "Leelawadee UI Bold",
+  },
+  LucidaConsole: {
+    value: "LucidaConsole",
+    label: "Lucida Console",
+  },
+  LucidaSansUnicode: {
+    value: "LucidaSansUnicode",
+    label: "Lucida Sans Unicode",
+  },
+  MalgunGothic: {
+    value: "MalgunGothic",
+    label: "Malgun Gothic",
+  },
+  MalgunGothicBold: {
+    value: "MalgunGothicBold",
+    label: "Malgun Gothic Bold",
+  },
+  MalgunGothicSemilight: {
+    value: "MalgunGothicSemilight",
+    label: "Malgun Gothic Semilight",
+  },
+  Marlett: {
+    value: "Marlett",
+    label: "Marlett",
+  },
+  MicrosoftHimalaya: {
+    value: "MicrosoftHimalaya",
+    label: "Microsoft Himalaya",
+  },
+  MicrosoftJhengHeiLight: {
+    value: "MicrosoftJhengHeiLight",
+    label: "Microsoft JhengHei Light",
+  },
+  MicrosoftJhengHei: {
+    value: "MicrosoftJhengHei",
+    label: "Microsoft JhengHei",
+  },
+  MicrosoftJhengHeiBold: {
+    value: "MicrosoftJhengHeiBold",
+    label: "Microsoft JhengHei Bold",
+  },
+  MicrosoftJhengHeiUILight: {
+    value: "MicrosoftJhengHeiUILight",
+    label: "Microsoft JhengHei UI Light",
+  },
+  MicrosoftJhengHeiUI: {
+    value: "MicrosoftJhengHeiUI",
+    label: "Microsoft JhengHei UI",
+  },
+  MicrosoftJhengHeiUIBold: {
+    value: "MicrosoftJhengHeiUIBold",
+    label: "Microsoft JhengHei UI Bold",
+  },
+  MicrosoftNewTaiLue: {
+    value: "MicrosoftNewTaiLue",
+    label: "Microsoft New Tai Lue",
+  },
+  MicrosoftNewTaiLueBold: {
+    value: "MicrosoftNewTaiLueBold",
+    label: "Microsoft New Tai Lue Bold",
+  },
+  MicrosoftPhagsPa: {
+    value: "MicrosoftPhagsPa",
+    label: "Microsoft PhagsPa",
+  },
+  MicrosoftPhagsPaBold: {
+    value: "MicrosoftPhagsPaBold",
+    label: "Microsoft PhagsPa Bold",
+  },
+  MicrosoftSansSerif: {
+    value: "MicrosoftSansSerif",
+    label: "Microsoft Sans Serif",
+  },
+  MicrosoftTaiLe: {
+    value: "MicrosoftTaiLe",
+    label: "Microsoft Tai Le",
+  },
+  MicrosoftTaiLeBold: {
+    value: "MicrosoftTaiLeBold",
+    label: "Microsoft Tai Le Bold",
+  },
+  MicrosoftYaHeiLight: {
+    value: "MicrosoftYaHeiLight",
+    label: "Microsoft YaHei Light",
+  },
+  MicrosoftYaHei: {
+    value: "MicrosoftYaHei",
+    label: "Microsoft YaHei",
+  },
+  MicrosoftYaHeiBold: {
+    value: "MicrosoftYaHeiBold",
+    label: "Microsoft YaHei Bold",
+  },
+  MicrosoftYaHeiUILight: {
+    value: "MicrosoftYaHeiUILight",
+    label: "Microsoft YaHei UI Light",
+  },
+  MicrosoftYaHeiUI: {
+    value: "MicrosoftYaHeiUI",
+    label: "Microsoft YaHei UI",
+  },
+  MicrosoftYaHeiUIBold: {
+    value: "MicrosoftYaHeiUIBold",
+    label: "Microsoft YaHei UI Bold",
+  },
+  MicrosoftYiBaiti: {
+    value: "MicrosoftYiBaiti",
+    label: "Microsoft Yi Baiti",
+  },
+  MingLiUExtB: {
+    value: "MingLiUExtB",
+    label: "MingLiU-ExtB",
+  },
+  PMingLiUExtB: {
+    value: "PMingLiUExtB",
+    label: "PMingLiU-ExtB",
+  },
+  MingLiUHKSCSExtB: {
+    value: "MingLiUHKSCSExtB",
+    label: "MingLiU_HKSCS-ExtB",
+  },
+  MongolianBaiti: {
+    value: "MongolianBaiti",
+    label: "Mongolian Baiti",
+  },
+  MSGothic: {
+    value: "MSGothic",
+    label: "MS Gothic",
+  },
+  MSPGothic: {
+    value: "MSPGothic",
+    label: "MS PGothic",
+  },
+  MSUIGothic: {
+    value: "MSUIGothic",
+    label: "MS UI Gothic",
+  },
+  MVBoli: {
+    value: "MVBoli",
+    label: "MV Boli",
+  },
+  MyanmarText: {
+    value: "MyanmarText",
+    label: "Myanmar Text",
+  },
+  MyanmarTextBold: {
+    value: "MyanmarTextBold",
+    label: "Myanmar Text Bold",
+  },
+  NirmalaUISemilight: {
+    value: "NirmalaUISemilight",
+    label: "Nirmala UI Semilight",
+  },
+  NirmalaUI: {
+    value: "NirmalaUI",
+    label: "Nirmala UI",
+  },
+  NirmalaUIBold: {
+    value: "NirmalaUIBold",
+    label: "Nirmala UI Bold",
+  },
+  PalatinoLinotype: {
+    value: "PalatinoLinotype",
+    label: "Palatino Linotype",
+  },
+  PalatinoLinotypeItalic: {
+    value: "PalatinoLinotypeItalic",
+    label: "Palatino Linotype Italic",
+  },
+  PalatinoLinotypeBold: {
+    value: "PalatinoLinotypeBold",
+    label: "Palatino Linotype Bold",
+  },
+  PalatinoLinotypeBoldItalic: {
+    value: "PalatinoLinotypeBoldItalic",
+    label: "Palatino Linotype Bold Italic",
+  },
+  SegoeMDL2Assets: {
+    value: "SegoeMDL2Assets",
+    label: "Segoe MDL2 Assets",
+  },
+  SegoePrint: {
+    value: "SegoePrint",
+    label: "Segoe Print",
+  },
+  SegoePrintBold: {
+    value: "SegoePrintBold",
+    label: "Segoe Print Bold",
+  },
+  SegoeScript: {
+    value: "SegoeScript",
+    label: "Segoe Script",
+  },
+  SegoeScriptBold: {
+    value: "SegoeScriptBold",
+    label: "Segoe Script Bold",
+  },
+  SegoeUILight: {
+    value: "SegoeUILight",
+    label: "Segoe UI Light",
+  },
+  SegoeUILightItalic: {
+    value: "SegoeUILightItalic",
+    label: "Segoe UI Light Italic",
+  },
+  SegoeUISemilight: {
+    value: "SegoeUISemilight",
+    label: "Segoe UI Semilight",
+  },
+  SegoeUISemilightItalic: {
+    value: "SegoeUISemilightItalic",
+    label: "Segoe UI Semilight Italic",
+  },
+  SegoeUI: {
+    value: "SegoeUI",
+    label: "Segoe UI",
+  },
+  SegoeUIItalic: {
+    value: "SegoeUIItalic",
+    label: "Segoe UI Italic",
+  },
+  SegoeUISemibold: {
+    value: "SegoeUISemibold",
+    label: "Segoe UI Semibold",
+  },
+  SegoeUISemiboldItalic: {
+    value: "SegoeUISemiboldItalic",
+    label: "Segoe UI Semibold Italic",
+  },
+  SegoeUIBold: {
+    value: "SegoeUIBold",
+    label: "Segoe UI Bold",
+  },
+  SegoeUIBoldItalic: {
+    value: "SegoeUIBoldItalic",
+    label: "Segoe UI Bold Italic",
+  },
+  SegoeUIBlack: {
+    value: "SegoeUIBlack",
+    label: "Segoe UI Black",
+  },
+  SegoeUIBlackItalic: {
+    value: "SegoeUIBlackItalic",
+    label: "Segoe UI Black Italic",
+  },
+  SegoeUIHistoric: {
+    value: "SegoeUIHistoric",
+    label: "Segoe UI Historic",
+  },
+  SegoeUIEmoji: {
+    value: "SegoeUIEmoji",
+    label: "Segoe UI Emoji",
+  },
+  SegoeUISymbol: {
+    value: "SegoeUISymbol",
+    label: "Segoe UI Symbol",
+  },
+  SimSun: {
+    value: "SimSun",
+    label: "SimSun",
+  },
+  NSimSun: {
+    value: "NSimSun",
+    label: "NSimSun",
+  },
+  SimSunExtB: {
+    value: "SimSunExtB",
+    label: "SimSun-ExtB",
+  },
+  SitkaSmall: {
+    value: "SitkaSmall",
+    label: "Sitka Small",
+  },
+  SitkaSmallItalic: {
+    value: "SitkaSmallItalic",
+    label: "Sitka Small Italic",
+  },
+  SitkaSmallBold: {
+    value: "SitkaSmallBold",
+    label: "Sitka Small Bold",
+  },
+  SitkaSmallBoldItalic: {
+    value: "SitkaSmallBoldItalic",
+    label: "Sitka Small Bold Italic",
+  },
+  SitkaText: {
+    value: "SitkaText",
+    label: "Sitka Text",
+  },
+  SitkaTextItalic: {
+    value: "SitkaTextItalic",
+    label: "Sitka Text Italic",
+  },
+  SitkaTextBold: {
+    value: "SitkaTextBold",
+    label: "Sitka Text Bold",
+  },
+  SitkaTextBoldItalic: {
+    value: "SitkaTextBoldItalic",
+    label: "Sitka Text Bold Italic",
+  },
+  SitkaSubheading: {
+    value: "SitkaSubheading",
+    label: "Sitka Subheading",
+  },
+  SitkaSubheadingItalic: {
+    value: "SitkaSubheadingItalic",
+    label: "Sitka Subheading Italic",
+  },
+  SitkaSubheadingBold: {
+    value: "SitkaSubheadingBold",
+    label: "Sitka Subheading Bold",
+  },
+  SitkaSubheadingBoldItalic: {
+    value: "SitkaSubheadingBoldItalic",
+    label: "Sitka Subheading Bold Italic",
+  },
+  SitkaHeading: {
+    value: "SitkaHeading",
+    label: "Sitka Heading",
+  },
+  SitkaHeadingItalic: {
+    value: "SitkaHeadingItalic",
+    label: "Sitka Heading Italic",
+  },
+  SitkaHeadingBold: {
+    value: "SitkaHeadingBold",
+    label: "Sitka Heading Bold",
+  },
+  SitkaHeadingBoldItalic: {
+    value: "SitkaHeadingBoldItalic",
+    label: "Sitka Heading Bold Italic",
+  },
+  SitkaDisplay: {
+    value: "SitkaDisplay",
+    label: "Sitka Display",
+  },
+  SitkaDisplayItalic: {
+    value: "SitkaDisplayItalic",
+    label: "Sitka Display Italic",
+  },
+  SitkaDisplayBold: {
+    value: "SitkaDisplayBold",
+    label: "Sitka Display Bold",
+  },
+  SitkaDisplayBoldItalic: {
+    value: "SitkaDisplayBoldItalic",
+    label: "Sitka Display Bold Italic",
+  },
+  SitkaBanner: {
+    value: "SitkaBanner",
+    label: "Sitka Banner",
+  },
+  SitkaBannerItalic: {
+    value: "SitkaBannerItalic",
+    label: "Sitka Banner Italic",
+  },
+  SitkaBannerBold: {
+    value: "SitkaBannerBold",
+    label: "Sitka Banner Bold",
+  },
+  SitkaBannerBoldItalic: {
+    value: "SitkaBannerBoldItalic",
+    label: "Sitka Banner Bold Italic",
+  },
+  Sylfaen: {
+    value: "Sylfaen",
+    label: "Sylfaen",
+  },
+  Symbol: {
+    value: "Symbol",
+    label: "Symbol",
+  },
+  Tahoma: {
+    value: "Tahoma",
+    label: "Tahoma",
+  },
+  TahomaBold: {
+    value: "TahomaBold",
+    label: "Tahoma Bold",
+  },
+  TimesNewRoman: {
+    value: "TimesNewRoman",
+    label: "Times New Roman",
+  },
+  TimesNewRomanItalic: {
+    value: "TimesNewRomanItalic",
+    label: "Times New Roman Italic",
+  },
+  TimesNewRomanBold: {
+    value: "TimesNewRomanBold",
+    label: "Times New Roman Bold",
+  },
+  TimesNewRomanBoldItalic: {
+    value: "TimesNewRomanBoldItalic",
+    label: "Times New Roman Bold Italic",
+  },
+  TrebuchetMS: {
+    value: "TrebuchetMS",
+    label: "Trebuchet MS",
+  },
+  TrebuchetMSItalic: {
+    value: "TrebuchetMSItalic",
+    label: "Trebuchet MS Italic",
+  },
+  TrebuchetMSBold: {
+    value: "TrebuchetMSBold",
+    label: "Trebuchet MS Bold",
+  },
+  TrebuchetMSBoldItalic: {
+    value: "TrebuchetMSBoldItalic",
+    label: "Trebuchet MS Bold Italic",
+  },
+  Verdana: {
+    value: "Verdana",
+    label: "Verdana",
+  },
+  VerdanaItalic: {
+    value: "VerdanaItalic",
+    label: "Verdana Italic",
+  },
+  VerdanaBold: {
+    value: "VerdanaBold",
+    label: "Verdana Bold",
+  },
+  VerdanaBoldItalic: {
+    value: "VerdanaBoldItalic",
+    label: "Verdana Bold Italic",
+  },
+  Webdings: {
+    value: "Webdings",
+    label: "Webdings",
+  },
+  Wingdings: {
+    value: "Wingdings",
+    label: "Wingdings",
+  },
+  YuGothicLight: {
+    value: "YuGothicLight",
+    label: "Yu Gothic Light",
+  },
+  YuGothicRegular: {
+    value: "YuGothicRegular",
+    label: "Yu Gothic Regular",
+  },
+  YuGothicMedium: {
+    value: "YuGothicMedium",
+    label: "Yu Gothic Medium",
+  },
+  YuGothicBold: {
+    value: "YuGothicBold",
+    label: "Yu Gothic Bold",
+  },
+  YuGothicUILight: {
+    value: "YuGothicUILight",
+    label: "Yu Gothic UI Light",
+  },
+  YuGothicUISemilight: {
+    value: "YuGothicUISemilight",
+    label: "Yu Gothic UI Semilight",
+  },
+  YuGothicUIRegular: {
+    value: "YuGothicUIRegular",
+    label: "Yu Gothic UI Regular",
+  },
+  YuGothicUISemibold: {
+    value: "YuGothicUISemibold",
+    label: "Yu Gothic UI Semibold",
+  },
+  YuGothicUIBold: {
+    value: "YuGothicUIBold",
+    label: "Yu Gothic UI Bold",
+  },
+  DengXianLight: {
+    value: "DengXianLight",
+    label: "DengXian Light",
+  },
+  DengXian: {
+    value: "DengXian",
+    label: "DengXian",
+  },
+  DengXianBold: {
+    value: "DengXianBold",
+    label: "DengXian Bold",
+  },
+  FangSong: {
+    value: "FangSong",
+    label: "FangSong",
+  },
+  KaiTi: {
+    value: "KaiTi",
+    label: "KaiTi",
+  },
+  SimHei: {
+    value: "SimHei",
+    label: "SimHei",
+  },
+};
