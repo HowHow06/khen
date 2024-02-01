969cd1f8582b8737598db72d03f64c0f360d1518
Howard <howard_bb@hotmail.com>
2024-02-01 18:33:54 +0800

    feat: cover settings UI

---
+++ components/ppt-generator/settings/CoverSettings.tsx
@@ -0,0 +1,65 @@
+import { PPT_GENERATION_SETTINGS_META, SETTING_CATEGORY } from "@/lib/constant";
+import { cn } from "@/lib/utils";
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../ui/form";
+import SettingInputField from "./SettingInputField";
+
+type CoverSettingsProps = {
+  contentKey?: string;
+};
+
+const CoverSettings = ({
+  contentKey,
+  className,
+}: CoverSettingsProps & React.HTMLAttributes<HTMLDivElement>) => {
+  const { control } = useFormContext();
+
+  const fieldNamePrefix =
+    SETTING_CATEGORY.COVER + "." + (contentKey ? contentKey + "." : "");
+  return (
+    <div className={cn("mr-2 grid divide-y py-2", className)}>
+      {Object.entries(PPT_GENERATION_SETTINGS_META.cover).map(
+        ([key, value]) => {
+          if (value.isHidden) {
+            return;
+          }
+          return (
+            <>
+              <FormField
+                control={control}
+                name={fieldNamePrefix + value.fieldKey}
+                key={fieldNamePrefix + value.fieldKey}
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-6 items-center gap-x-3 py-4">
+                    <FormLabel className="col-span-4 text-left text-sm">
+                      {value.fieldDisplayName}
+                    </FormLabel>
+                    <FormControl>
+                      <SettingInputField
+                        settingItemMeta={value}
+                        field={field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription className="col-span-6">
+                      This is description.
+                    </FormDescription> */}
+                    <FormMessage className="col-span-6 " />
+                  </FormItem>
+                )}
+              />
+              {/* <Label htmlFor={value.fieldSlug}>{value.fieldDisplayName}</Label> */}
+            </>
+          );
+        },
+      )}
+    </div>
+  );
+};
+
+export default CoverSettings;
