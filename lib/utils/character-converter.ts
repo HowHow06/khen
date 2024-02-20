e1a7f6fbd2ad7be6696812f19cbebc1cc995f3e6
Howard <howard_bb@hotmail.com>
2024-02-21 00:59:36 +0800

    feat: add custom traditional to simplified dict

--- lib/utils/character-converter.ts
+++ lib/utils/character-converter.ts
@@ -1,9 +1,16 @@
 import * as OpenCC from "opencc-js";
 
+const customTraditionalToSimplifiedDict: [string, string][] = [
+  ["罣虑", "挂虑"],
+];
+
 export function convertToSimplified(text: string) {
   const converter = OpenCC.Converter({ from: "tw", to: "cn" });
   const convertedText = converter(text);
-  return convertedText;
+  const customConverter = OpenCC.CustomConverter(
+    customTraditionalToSimplifiedDict,
+  );
+  return customConverter(convertedText);
 }
 
 export function convertToTraditional(text: string) {
