b36979b8688b84c1d56e5d1b7e2e061b192e08d3
Howard <howard_bb@hotmail.com>
2024-03-13 14:22:28 +0800

    feat: trim each line for generated pinyin

--- lib/utils/pinyin.ts
+++ lib/utils/pinyin.ts
@@ -7,16 +7,22 @@ customPinyin(CUSTOM_PINYIN_MAP);
 export const getPinyin = ({
   text,
   hasTone = false,
+  trimEachLine = true,
 }: {
   text: string;
   hasTone: boolean;
+  trimEachLine?: boolean;
 }): string => {
   // the 多音多义字 only works with simplified chinese character, therefore need to convert to simplified chinese before generating pinyin
   const simplifiedText = convertToSimplified(text);
-  const pinyinText = pinyin(simplifiedText, {
+  let pinyinText = pinyin(simplifiedText, {
     pattern: "pinyin",
     nonZh: "consecutive",
     toneType: hasTone ? "symbol" : "none",
   });
+  if (trimEachLine) {
+    const lines = pinyinText.split(/\r?\n/);
+    pinyinText = lines.map((line) => line.trim()).join(`\n`);
+  }
   return pinyinText;
 };
