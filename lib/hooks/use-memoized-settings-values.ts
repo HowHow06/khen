f8b1aae99b6aca08d8e8f858af09bbad52f9e840
Howard <howard_bb@hotmail.com>
2024-08-30 19:52:24 +0800

    feat: debounce new settings values

--- lib/hooks/use-memoized-settings-values.ts
+++ lib/hooks/use-memoized-settings-values.ts
@@ -1,6 +1,7 @@
 import { useMemo, useRef } from "react";
 import { PptSettingsStateType } from "../types";
 import { deepCompare, deepCopy } from "../utils";
+import useDebouncedValue from "./use-debounced-value";
 
 type Props = {
   newSettingsValues: PptSettingsStateType;
@@ -8,23 +9,30 @@ type Props = {
 
 const useMemoizedSettingsValues = ({ newSettingsValues }: Props) => {
   const existingSettingsValuesRef = useRef<PptSettingsStateType | null>(null);
+  const debouncedNewSettingsValues = useDebouncedValue(newSettingsValues, 300);
   // TODO: can debounce?
   const settingsValues = useMemo(() => {
     // If prevSettingsValuesRef is null (first render),
     // or if the new values are different, update the ref and return the new values
     if (
       existingSettingsValuesRef.current === null ||
-      !deepCompare(existingSettingsValuesRef.current, newSettingsValues)
+      !deepCompare(
+        existingSettingsValuesRef.current,
+        debouncedNewSettingsValues,
+      )
     ) {
       if (process.env.NODE_ENV === "development") {
-        console.log("Settings Value Reference Updated:", newSettingsValues);
+        console.log(
+          "Settings Value Reference Updated:",
+          debouncedNewSettingsValues,
+        );
       }
-      existingSettingsValuesRef.current = deepCopy(newSettingsValues);
+      existingSettingsValuesRef.current = deepCopy(debouncedNewSettingsValues);
     }
 
     // Return the current (or previous) values stored in the ref
     return existingSettingsValuesRef.current;
-  }, [newSettingsValues]);
+  }, [debouncedNewSettingsValues]);
 
   return settingsValues;
 };
