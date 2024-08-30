e91969ee5f5550bc0d672a0c185b884e3d979d2c
Howard <howard_bb@hotmail.com>
2024-08-30 13:07:49 +0800

    fix: use deep copy for previous

--- lib/hooks/use-memoized-settings-values.ts
+++ lib/hooks/use-memoized-settings-values.ts
@@ -1,33 +1,41 @@
 import { useMemo, useRef } from "react";
 import { UseFormGetValues } from "react-hook-form";
 import { PptSettingsStateType } from "../types";
-import { deepCompare } from "../utils";
+import { deepCompare, deepCopy } from "../utils";
 
 type Props = {
   valuesGetter: UseFormGetValues<{
     [x: string]: any;
   }>;
+  onValuesChange?: (
+    prevValues: PptSettingsStateType | null,
+    newValues: PptSettingsStateType,
+  ) => void;
 };
 
-const useMemoizedSettingsValues = ({ valuesGetter }: Props) => {
-  const prevSettingsValuesRef = useRef<PptSettingsStateType | null>(null);
+const useMemoizedSettingsValues = ({ valuesGetter, onValuesChange }: Props) => {
+  const existingSettingsValuesRef = useRef<PptSettingsStateType | null>(null);
   const newSettingsValues = valuesGetter() as PptSettingsStateType;
 
   const settingsValues = useMemo(() => {
-    // If prevSettingsValuesRef is null (first render), or if the new values are different, update the ref and return the new values
+    // If prevSettingsValuesRef is null (first render),
+    // or if the new values are different, update the ref and return the new values
     if (
-      prevSettingsValuesRef.current === null ||
-      !deepCompare(prevSettingsValuesRef.current, newSettingsValues)
+      existingSettingsValuesRef.current === null ||
+      !deepCompare(existingSettingsValuesRef.current, newSettingsValues)
     ) {
       if (process.env.NODE_ENV === "development") {
         console.log("Settings Value Reference Updated:", newSettingsValues);
       }
-      prevSettingsValuesRef.current = newSettingsValues;
+      if (onValuesChange) {
+        onValuesChange(existingSettingsValuesRef.current, newSettingsValues);
+      }
+      existingSettingsValuesRef.current = deepCopy(newSettingsValues);
     }
 
     // Return the current (or previous) values stored in the ref
-    return prevSettingsValuesRef.current;
-  }, [newSettingsValues]);
+    return existingSettingsValuesRef.current;
+  }, [newSettingsValues, onValuesChange]);
 
   return settingsValues;
 };
